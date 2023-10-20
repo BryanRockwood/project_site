4f419654eab6b50a3aaf75a831303c77fb4b2120
Bryan Rockwood <bryanprockwood@gmail.com>
2023-10-20 13:22:20 -0400

    added js script forf search bar

---
+++ playersearch.js
@@ -0,0 +1,10 @@
+const testArray = ['test', 'second'];
+
+function playerCompare(){
+	let player  = document.getElementById("playerA").value;
+//	const resultList = document.createElement('li')
+//	resultList.classList.add('result')
+//	resultList.appendChild(player)
+//	list.appendChild(resultList)
+	document.getElementById("inputResult").innerHTML = player;
+}
